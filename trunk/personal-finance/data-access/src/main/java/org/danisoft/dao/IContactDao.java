974a175b382b83608517519fa588f1728daf02fa
danigpino@gmail.com <danigpino@gmail.com@eb48dcc6-5f3e-11de-bbaa-f1a44a2b9d47>
2012-01-18 16:39:19 +0000

    new daos + implementation

---
+++ trunk/personal-finance/data-access/src/main/java/org/danisoft/dao/IContactDao.java
@@ -0,0 +1,7 @@
+package org.danisoft.dao;
+
+import org.danisoft.model.Contact;
+
+public interface IContactDao extends IReadDao<Contact>, IWriteDao<Contact>{
+
+}
---
+++ trunk/personal-finance/data-access/src/main/java/org/danisoft/dao/mock/ContactDaoMock.java
@@ -0,0 +1,40 @@
+package org.danisoft.dao.mock;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.danisoft.dao.IContactDao;
+import org.danisoft.model.Contact;
+
+public class ContactDaoMock  implements IContactDao {
+	
+	private Map<Integer, Contact> data = new HashMap<Integer, Contact>();
+	private int idSequence = 0;
+
+	public Contact get(int id) {
+		return data.get(id);
+	}
+
+	public List<Contact> getAll() {
+		return new ArrayList<Contact>(data.values());
+	}
+
+	public int save(Contact object) {
+		idSequence++;
+		object.setId(idSequence);
+		data.put(idSequence, object);
+		
+		return idSequence;
+	}
+
+	public void update(Contact object) {
+		data.put(object.getId(), object);
+	}
+
+	public void delete(Contact object) {
+		data.remove(object.getId());
+	}
+
+}
